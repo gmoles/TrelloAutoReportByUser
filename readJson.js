2029124adc33c01f65b9a0c3341ba4e377ec4fb2
gmoles <guilherme.moles@gmail.com>
2021-09-10 12:52:06 -0300

    LogicFinished

--- readJson.js
+++ readJson.js
@@ -1,5 +1,42 @@
 var fs = require("fs");
 
+ 
+var reportItiActivities = [];
+
+var vData = null;
+var vSistema = null;
+var vAtividadeRealizada = null;
+var vIniAtividade = null;
+var vFimAtividade = null;
+var vPrazo = null;
+var vStatus = null;
+var vResponsavel = null;
+var vObservacoes = null;
+
+
+var activity = {
+  data : null,
+  sistema : null,
+  atividadeRealizada : null,
+  iniAtividade : null, 
+  fimAtividade : null,
+  prazo : null,
+  status : null,
+  responsavel : null,
+  observacoes : null,
+}
+
+function Activities(data, sistema, atividadeRealizada, iniAtividade, fimAtividade, prazo, status, responsavel, observacoes) {
+  this.data = data;
+  this.sistema = sistema;
+  this.atividadeRealizada = atividadeRealizada;
+  this.iniAtividade = iniAtividade;
+  this.fimAtividade = fimAtividade;
+  this.prazo = prazo;
+  this.status = status;
+  this.responsavel = responsavel;
+  this.observacoes = observacoes;
+}
 
 fs.readFile("./repositJson/my-file.json" , "utf8", function(err, data){
   if(err){
@@ -42,20 +79,20 @@ fs.readFile("./repositJson/my-file.json" , "utf8", function(err, data){
         if (typeof idMemberCard === "undefined"){
           //console.log('Card não pertence ao Membro')
         } else {
+          
+          if (idMemberCard === members[item].id && !card.closed){
+            console.log("   Cartão " + card.name);
             
-            if (idMemberCard === members[item].id && !card.closed){
-              console.log("   Cartão " + card.name);
-              
-              //Verificar Movimentações de Cartão
-              actions.forEach((action) => {
-            
-                //Verifica se a ação do quadro é referente aos cartões
-                if (action.type.indexOf('updateCard') > -1 || action.type.indexOf('createCard') > -1){
-                  
-                  var idActionCard = action.data.card.id;
-                  
-                  //Verifica se a ação pertence ao Card
-                  if (card.id === idActionCard){
+            //Verificar Movimentações de Cartão
+            actions.forEach((action) => {
+          
+              //Verifica se a ação do quadro é referente aos cartões
+              if (action.type.indexOf('updateCard') > -1 || action.type.indexOf('createCard') > -1){
+                
+                var idActionCard = action.data.card.id;
+                
+                //Verifica se a ação pertence ao Card
+                if (card.id === idActionCard){
                   
                   //Tenta colocar na variavel o nome da Lista
                   try {
@@ -73,12 +110,67 @@ fs.readFile("./repositJson/my-file.json" , "utf8", function(err, data){
                   }
                   console.log("        " + listName + " " + listNameBefore + " " + action.date);
 
+                  if (listName === "Doing" && vData === null) {
+                    try {
+                      vSistema = card.labels[0].name;                                            
+                    } catch ({ message }) {
+                      vSistema = undefined;
+                    }                    
 
-                  } else {}
-                  }
-              });
-            }       
-        }
+                    vData = action.date;
+                    vAtividadeRealizada = card.name;
+                    vIniAtividade = action.date;
+                    vStatus = "Pendente";
+                    vResponsavel = members[item].fullName;
+                    vObservacoes = card.desc;
+                    
+                  } else if( listName != "Doing" &&  vData != null) {
+                      
+                      vFimAtividade = action.date;
+                      if (listName === "Done"){
+                        vStatus = "Feito";
+                      }
+                                        
+                      activity = new Activities(vData, vSistema, vAtividadeRealizada, vIniAtividade, vFimAtividade, vPrazo, vStatus, vResponsavel, vObservacoes);                  
+                      reportItiActivities.push(activity);
+
+                      //Zera as Variáveis
+                      vData = null;
+                      vSistema = null;
+                      vAtividadeRealizada = null;
+                      vIniAtividade = null;
+                      vFimAtividade = null;
+                      vPrazo = null;
+                      vStatus = null;
+                      vResponsavel = null;
+                      vObservacoes = null;
+                    }                  
+                } //if (card.id === idActionCard)
+              }
+            });
+            if (vData != null) {
+              activity = new Activities(vData, vSistema, vAtividadeRealizada, vIniAtividade, vFimAtividade, vPrazo, vStatus, vResponsavel, vObservacoes);                  
+              reportItiActivities.push(activity);
+              
+              //Zera as Variáveis
+              vData = null;
+              vSistema = null;
+              vAtividadeRealizada = null;
+              vIniAtividade = null;
+              vFimAtividade = null;
+              vPrazo = null;
+              vStatus = null;
+              vResponsavel = null;
+              vObservacoes = null;
+
+            }
+          }       
+        } 
     });
   })
+  console.log(reportItiActivities);
+
+  console.log("fim");
+
 });  
+
